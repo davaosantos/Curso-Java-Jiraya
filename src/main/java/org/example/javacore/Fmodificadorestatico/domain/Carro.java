d98da043e8221c06c933f14d29e67bcc88a86717
david <david_Alisson60@yahoo.com.br>
2024-05-19 11:41:50 -0300

    Aula 61 - Métodos estáticos

---
+++ src/main/java/org/example/javacore/Fmodificadorestatico/domain/Carro.java
@@ -0,0 +1,42 @@
+package org.example.javacore.Fmodificadorestatico.domain;
+
+public class Carro {
+
+    private String nome;
+
+    private double velocidadeMaxima;
+
+    public static double velocidadeLimite = 250;
+
+    public void imprime(){
+        System.out.println("------");
+        System.out.println("Nome : " + this.nome);
+        System.out.println("Velocidade Máxima : " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite : " + Carro.velocidadeLimite);
+    }
+
+    public Carro() {
+    }
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+}
