f8165e2e7e103738d0a331f7a8e319da12f70d26
David Alisson <david@infotera.com.br>
2024-07-08 21:14:00 -0300

    Até aula 81 - Enum pt 02 - Construtores e métodos .

--- src/main/java/org/example/javacore/KEnum/dominio/TipoCliente.java
+++ src/main/java/org/example/javacore/KEnum/dominio/TipoCliente.java
@@ -1,6 +1,24 @@
 package org.example.javacore.KEnum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    private int valor;
+
+    private String nmRelatorio;
+
+    TipoCliente(int valor, String nmRelatorio) {
+        this.valor = valor;
+        this.nmRelatorio = nmRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNmRelatorio() {
+        return nmRelatorio;
+    }
 }
--- src/main/java/org/example/javacore/KEnum/dominio/test/ClienteTest01.java
+++ src/main/java/org/example/javacore/KEnum/dominio/test/ClienteTest01.java
@@ -3,22 +3,20 @@ package org.example.javacore.KEnum.dominio.test;
 import org.example.javacore.KEnum.dominio.Cliente;
 import org.example.javacore.KEnum.dominio.TipoCliente;
 
+import static org.example.javacore.KEnum.dominio.Cliente.TipoPagamento.CREDITO;
+import static org.example.javacore.KEnum.dominio.Cliente.TipoPagamento.DEBITO;
+
 public class ClienteTest01 {
 
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("TSUBASA",
-                TipoCliente.PESSOA_FISICA);
+        Cliente cliente1 = new Cliente("SHIRYU",
+                TipoCliente.PESSOA_FISICA, DEBITO);
 
         Cliente cliente2 = new Cliente("SEYA",
-                TipoCliente.PESSOA_JURIDICA);
-
-        Cliente cliente3 = new Cliente("ROLIN",
-                TipoCliente.PESSOA_FISICA);
-
+                TipoCliente.PESSOA_JURIDICA, CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
 
     }
 }
