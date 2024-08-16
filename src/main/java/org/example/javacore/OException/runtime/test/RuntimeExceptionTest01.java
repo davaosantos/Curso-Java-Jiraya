9e4fe24e099c09fa53f5f266ef726d698e7af1ff
david <david_Alisson60@yahoo.com.br>
2024-08-15 22:19:37 -0300

    96 - Exceções pt 02 - RuntimeException

---
+++ src/main/java/org/example/javacore/OException/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,12 @@
+package org.example.javacore.OException.runtime.test;
+
+public class RuntimeExceptionTest01 {
+
+    //Checked e Unchecked
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(nums[4]);
+    }
+
+}
