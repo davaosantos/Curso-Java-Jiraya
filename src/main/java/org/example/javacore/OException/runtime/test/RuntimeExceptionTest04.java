67a20e1076fcf7471b4ec0f94f43c841ebd38038
david <david_Alisson60@yahoo.com.br>
2024-08-29 22:04:16 -0300

    102 - Capturando multiplas excecoes

--- src/main/java/org/example/javacore/OException/runtime/test/RuntimeExceptionTest04.java
+++ src/main/java/org/example/javacore/OException/runtime/test/RuntimeExceptionTest04.java
@@ -5,25 +5,25 @@ import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
 
         try {
             throw new ArrayIndexOutOfBoundsException();
-        }catch (ArrayIndexOutOfBoundsException e){
+        }catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException  e){
             System.out.println("Dentro do arrayExp");
         }catch (IndexOutOfBoundsException e){
             System.out.println("Dentro do index of bound");
-        }catch (IllegalArgumentException e){
-            System.out.println("Illegal exc");
-        }catch (ArithmeticException ex){
-            System.out.println("Aritim exc");
         }catch (RuntimeException ex){
 
         }
 
         try {
             talvezLanceException();
-        } catch (Exception ex){}
+        } catch (SQLException e){
+
+        } catch (FileNotFoundException e){
+            
+        }
     }
 
     private static void talvezLanceException() throws SQLException, FileNotFoundException{
