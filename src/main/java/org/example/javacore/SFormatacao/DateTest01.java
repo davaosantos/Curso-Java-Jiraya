385fea9b25ae32f8874208ffef1ccd56a4b274dd
david <david_Alisson60@yahoo.com.br>
2024-09-22 17:06:39 -0300

    116 - Classes uti - Internacionalizacao com Locale

---
+++ src/main/java/org/example/javacore/SFormatacao/DateTest01.java
@@ -0,0 +1,12 @@
+package org.example.javacore.SFormatacao;
+
+import java.util.Date;
+
+public class DateTest01 {
+
+    public static void main(String[] args) {
+        Date data = new Date(1726105465315L); // Retorna em mili segundos , desde 1970
+        System.out.println(data);
+    }
+
+}
---
+++ src/main/java/org/example/javacore/SFormatacao/NumberFormatTest01.java
@@ -0,0 +1,28 @@
+package org.example.javacore.SFormatacao;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+
+    public static void main(String[] args) {
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+        NumberFormat[] nfa = new NumberFormat[4];
+
+        nfa[0] = NumberFormat.getInstance();
+        nfa[1] = NumberFormat.getInstance(localeBR);
+        nfa[2] = NumberFormat.getInstance(localeIT);
+        nfa[3] = NumberFormat.getInstance(localeJP);
+
+        Double valor = 10_000.2130;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+
+    }
+}
