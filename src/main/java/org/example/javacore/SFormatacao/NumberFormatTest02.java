d56db4ee2b928b2223d8921dded354da6168baa6
david <david_Alisson60@yahoo.com.br>
2024-09-22 17:32:33 -0300

    117 - Classes Utilitárias - Internacionalização de moeda com Locale

--- src/main/java/org/example/javacore/SFormatacao/NumberFormatTest02.java
+++ src/main/java/org/example/javacore/SFormatacao/NumberFormatTest02.java
@@ -4,35 +4,30 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
 
     public static void main(String[] args) {
         Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
-//        Locale[] localeArr = Locale.getAvailableLocales();
-
-//        for (Locale locale : localeArr) {
-//            System.out.print(locale.getDisplayLanguage());
-//            System.out.println();
-//            System.out.print(locale.getDisplayCountry());
-//        }
-
 
         NumberFormat[] nfa = new NumberFormat[4];
 
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeBR);
-        nfa[2] = NumberFormat.getInstance(localeIT);
-        nfa[3] = NumberFormat.getInstance(localeJP);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeIT);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeJP);
 
         Double valor = 10_000.2130;
 
         for (NumberFormat numberFormat : nfa) {
+//            System.out.println(numberFormat.getMaximumIntegerDigits());
+            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
+            System.out.println(numberFormat.getMaximumFractionDigits());
         }
 
-        String valorString = "$1_000.2130";
+        String valorString = "1_000.2130";
         try {
             System.out.println(nfa[0].parse(valorString));
         } catch (ParseException e) {
