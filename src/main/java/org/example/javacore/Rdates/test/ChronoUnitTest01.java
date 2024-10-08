b14904bab8cbc1cf3e72e305142719a1dea99abb
david <david_Alisson60@yahoo.com.br>
2024-10-07 21:40:45 -0300

    125 -> ChronoUnit Classes Uti

---
+++ src/main/java/org/example/javacore/Rdates/test/ChronoUnitTest01.java
@@ -0,0 +1,18 @@
+package org.example.javacore.Rdates.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(1998, Month.FEBRUARY, 21, 12 , 0 , 0);
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
