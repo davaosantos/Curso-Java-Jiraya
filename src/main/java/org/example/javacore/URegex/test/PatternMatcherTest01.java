21581b00f311c1738f630a5efd059bcf7910e302
david <david_Alisson60@yahoo.com.br>
2024-10-19 19:31:07 -0300

    131 - Regex pt 01

---
+++ src/main/java/org/example/javacore/URegex/test/PatternMatcherTest01.java
@@ -0,0 +1,22 @@
+package org.example.javacore.URegex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest01 {
+    public static void main(String[] args) {
+        String regex = "ab";
+        String text = "abaaba";
+        Pattern pattern = Pattern.compile("regex");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println("texto:   " + text);
+        System.out.println("Indice : 12345789");
+        System.out.println("Regex " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()){
+            System.out.println(matcher.start() + " ");
+        }
+
+    }
+}
