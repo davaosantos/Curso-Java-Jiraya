4bd4669e1db9dc80f953611f5c7cddd3d482853e
david <david_Alisson60@yahoo.com.br>
2024-10-28 22:05:00 -0300

    Aula 159 - Classes Utis - Serialization pt 01

---
+++ src/main/java/org/example/javacore/XSerializacao/test/SerializacaoTest01.java
@@ -0,0 +1,37 @@
+package org.example.javacore.XSerializacao.test;
+
+import org.example.javacore.XSerializacao.dominio.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "David", "repolho123");
+//        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar(Aluno aluno){
+        Path path = Paths.get("src/pasta/alunos.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException exception){
+
+        }
+    }
+
+    private static void deserializar(){
+        Path path = Paths.get("src/pasta/alunos.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))){
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        }catch (IOException | ClassNotFoundException exception){
+
+        }
+    }
+}
