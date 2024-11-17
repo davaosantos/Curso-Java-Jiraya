3ed4451f6af7f5cf0ab2e3b6549c65e5eea10edb
david <david_Alisson60@yahoo.com.br>
2024-11-17 12:20:28 -0300

    167 - Colecoes pt 07 - List pt 02

--- src/main/java/org/example/javacore/YColecoes/test/ListTest01.java
+++ src/main/java/org/example/javacore/YColecoes/test/ListTest01.java
@@ -8,8 +8,32 @@ public class ListTest01 {
     public static void main(String[] args) {
 //        List nomes = new ArrayList<>(); //1.4
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("David");
         nomes.add("Angelina");
+        nomes.add("Seila");
+
+        nomes2.add("Suane");
+        nomes2.add("Jhonis");
+        nomes2.add("Willis");
+
+        nomes.addAll(nomes2);
+
+        nomes.remove(0);
+        System.out.println(nomes.remove("Seila"));
+
+
+//        for (String nome : nomes) {
+//            nomes.add(nome);
+//        }
+
+        int sizeNomes = nomes.size();
+        for (int i = 0; i < sizeNomes; i++){
+            nomes.add("David");
+        }
+
+        System.out.println(nomes);
+
 
 //        for (Object nome : nomes) {
 //            System.out.println(nome);
@@ -25,6 +49,11 @@ public class ListTest01 {
         for (int i = 0; i < nomes.size() ; i++){
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+
+//        List<int>
 
     }
 }
