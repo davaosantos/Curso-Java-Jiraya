173b61a82be73fb7a24acb278f1ad31078480d8b
david <david_Alisson60@yahoo.com.br>
2024-12-07 00:49:56 -0300

    184 - Generics pt 02 - Wildcard pt 01

--- src/main/java/org/example/javacore/Zgenerics/test/WildCardTest02.java
+++ src/main/java/org/example/javacore/Zgenerics/test/WildCardTest02.java
@@ -9,7 +9,7 @@ public class WildCardTest02 {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
 
-        printConsulta(cachorros);
+//        printConsulta(cachorros);
         System.out.println("---------");
     }
 
