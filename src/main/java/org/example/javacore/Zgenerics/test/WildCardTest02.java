d32f2c6417e1b61a08a52e899ac11210b91d0efa
david <david_Alisson60@yahoo.com.br>
2024-12-07 13:45:36 -0300

    185 - Generics pt 03 - Wildcard pt 02

--- src/main/java/org/example/javacore/Zgenerics/test/WildCardTest02.java
+++ src/main/java/org/example/javacore/Zgenerics/test/WildCardTest02.java
@@ -1,6 +1,7 @@
 package org.example.javacore.Zgenerics.test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildCardTest02 {
@@ -9,11 +10,14 @@ public class WildCardTest02 {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
 
-//        printConsulta(cachorros);
+        printConsulta(cachorros);
         System.out.println("---------");
     }
 
-    private static void printConsulta(List<Animal> animals){
+    private static void printConsulta(List<? extends Animal> animals){
+
+        Animal animal = new Cachorro();
+        Cachorro cachorro = new Animal();
         for (Animal animal : animals) {
             System.out.println(animal);
         }
@@ -21,5 +25,33 @@ public class WildCardTest02 {
         Animal animal = new Cachorro();
         animals.add(new Cachorro());
 
+        List<Animal> animals1 = new ArrayList<>();
+        List<Cachorro> cachorros = new ArrayList<>();
+        List<Object> objects = new ArrayList<>();
+        printConsultaAnimal(animals1);
+        printConsultaAnimal(cachorros);
+//        printConsultaAnimal(objects);
+
     }
+
+    private static void printConsultaAnimal(List<? super Animal> animals){
+
+        Animal animalCac = new Cachorro();
+        Animal animalGato = new Gato();
+        animals.add(animalGato);
+        animals.add(animalCac);
+
+    }
+
+//    private static void printConsultaAnimal(List<? super Cachorro> animals){
+//
+//        for (Object animal : animals) {
+//            if (animal instanceof Cachorro){
+//
+//            }
+//        }
+//
+//    }
+
+
 }
