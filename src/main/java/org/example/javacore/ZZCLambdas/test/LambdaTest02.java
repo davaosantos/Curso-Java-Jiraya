644ae7d33aefff4eb76430dd26c12d748c5ca9e2
david <david_Alisson60@yahoo.com.br>
2024-12-22 12:49:05 -0300

    197 - Lambdas pt 03 - Function

---
+++ src/main/java/org/example/javacore/ZZCLambdas/test/LambdaTest02.java
@@ -0,0 +1,28 @@
+package org.example.javacore.ZZCLambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+
+        List<String> strings = List.of("Naruto", "Nanatsu", "Dragonball");
+        List<Integer> sizeString = map(strings, (String s) -> s.length());
+        List<String> strUppercase = map(strings, s -> s.toUpperCase());
+        System.out.println(sizeString);
+        System.out.println(strUppercase);
+
+    }
+
+    private static <T, R> List<R> map(List<T> tlist, Function<T,R> function){
+        List<R> result = new ArrayList<>();
+
+        for (T e : tlist) {
+            R r = function.apply(e);
+            result.add(r);
+        }
+
+        return result;
+    }
+}
