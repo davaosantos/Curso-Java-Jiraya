f0149fa48fa73b1e78f2beaedf7745a67af4d18c
david <david_Alisson60@yahoo.com.br>
2024-12-23 17:47:31 -0300

    201 - Optional pt 01

---
+++ src/main/java/org/example/javacore/ZZDOptional/test/OptionalTest01.java
@@ -0,0 +1,40 @@
+package org.example.javacore.ZZDOptional.test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class OptionalTest01 {
+
+    public static void main(String[] args) {
+        Optional<String> o1 = Optional.of("Aha uhu o devdojo é f");
+        Optional<String> o2 = Optional.ofNullable(null);
+        Optional<String> o3 = Optional.empty();
+        System.out.println(o1);
+        System.out.println(o2);
+
+        System.out.println("--------OPTIONAL VAZIO -----");
+        Optional<String> nameOptional = Optional.ofNullable(findName("Alisson"));
+        System.out.println(nameOptional);
+
+        String vazio = nameOptional.orElse("Vazio");
+        System.out.println(vazio);
+
+        System.out.println("--------- OPTIONAL ENCONTRADO -------");
+        Optional<String> nameOptionalEncontrado = Optional.ofNullable(findName("Sasuke"));
+        System.out.println(nameOptionalEncontrado);
+        nameOptionalEncontrado.ifPresent(s -> System.out.println(s.toUpperCase()));
+
+    }
+
+    private static Optional<String> findName(String name){
+        List<String> nomes = List.of("Naruto", "Sasuke", "Megumi", "Itadori");
+        int i = nomes.indexOf(name);
+
+        if (i != -1){ // IndexOf -> 0 é que encontrou , -1 nao encontrou
+            return Optional.of(nomes.get(i));
+        }
+
+        return Optional.empty();
+    }
+}
