ad64e3b4e94b202d4b4e2d90fb74fbe434fcd0de
david <david_Alisson60@yahoo.com.br>
2025-11-06 20:05:23 -0300

    250 - Padrões de Projeto pt 05 - Singleton pt 03 - Singleton with enumeration

---
+++ src/main/java/org/example/javacore/ZZHPadroesdeprojeto/dominio/AircraftSingletonEnum.java
@@ -0,0 +1,19 @@
+package org.example.javacore.ZZHPadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AircraftSingletonEnum {
+    INSTANCE;
+    private Set<String> availableSeats;
+
+    AircraftSingletonEnum() {
+        this.availableSeats = new HashSet<>();
+        this.availableSeats.add("1A");
+        this.availableSeats.add("1B");
+    }
+
+    public boolean bookSeat(String seat){
+        return availableSeats.remove(seat);
+    }
+}
